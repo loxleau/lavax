13015598e1376f0129a0738c167d5a2e654c25eb
loxleau <dikarvz@gmail.com>
2024-03-21 19:22:36 +0000

    🟣 auto commit

--- Update.tsx
+++ Update.tsx
@@ -1,1 +1,2 @@
   
+{"error":"{\"Error_GUID\":\"14589549849043524351\",\"Error\":\"getSupportedApi jsonrpc failed {reason:api not supported {name:eth_getBlockByNumber,connectionType:POST},method:eth_getBlockByNumber}\"}"}
