8e2b71c57dbbed1ba1cb5d98ddebe668e00055a2
loxleu <82130260+loxleau@users.noreply.github.com>
2024-03-22 01:35:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-65d26f19-c7a4-4cbb-aabe-3a93c76519a9/)
 
 
 echo "$curl_output" >> Update.tsx
