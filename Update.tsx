e0d0e7ec801d0b0a02eb85a7b7e9fbefd1a0681a
loxleau <dikarvz@gmail.com>
2024-03-23 23:12:18 +0000

    🟠 auto commit

--- Update.tsx
+++ Update.tsx
@@ -182,3 +182,4 @@
 {"error":"{\"Error_GUID\":\"1977244346656057550\",\"Error\":\"getSupportedApi jsonrpc failed {reason:api not supported {name:eth_getBlockByNumber,connectionType:POST},method:eth_getBlockByNumber}\"}"}
 {"error":"{\"Error_GUID\":\"17061134174942031652\",\"Error\":\"getSupportedApi jsonrpc failed {reason:api not supported {name:eth_getBlockByNumber,connectionType:POST},method:eth_getBlockByNumber}\"}"}
 {"error":"{\"Error_GUID\":\"2467469247395786805\",\"Error\":\"getSupportedApi jsonrpc failed {reason:api not supported {name:eth_getBlockByNumber,connectionType:POST},method:eth_getBlockByNumber}\"}"}
+{"error":"{\"Error_GUID\":\"16661325144327633892\",\"Error\":\"getSupportedApi jsonrpc failed {reason:api not supported {name:eth_getBlockByNumber,connectionType:POST},method:eth_getBlockByNumber}\"}"}
